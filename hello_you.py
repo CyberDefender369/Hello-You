33acebe1624a2675a01056f717d18b4392f78517
Graciano <96165986+CyberDefender369@users.noreply.github.com>
2023-10-31 18:38:11 -0700

    Add files via upload

---
+++ hello_you.py
@@ -0,0 +1,17 @@
+#ask user's name
+name = input("What is your name?: ")
+
+#ask user's age
+age = input("What is your age?: ")
+
+#ask user where they reside
+city = input("What city do you live in?: ")
+
+#ask user what they like doing in their free time
+hobby = input("What do you enjoy doing with your free time?: ")
+
+#create output 
+output_string = f"Your name is {name} and you are {age} years old. You live in {city} and you enjoy {hobby}."
+
+#print output
+print(output_string)
